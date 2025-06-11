8b704d536f9f1ca0ff1e02b048cfd3deb84e1690
Imotep92 <Imogyn.nicholas@yahoo.co.uk>
2025-06-11 02:57:41 +0100

    3rd Commit, 11/06/25, 02:57am
    - added score function, with updates whilst game is running

--- IN_Week6_Mobile_Game/Assets/Scripts/GameManger.cs
+++ IN_Week6_Mobile_Game/Assets/Scripts/GameManger.cs
@@ -1,19 +1,34 @@
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro.EditorUtilities;
+using UnityEngine.SocialPlatforms.Impl;
+
 
 public class GameManger : MonoBehaviour
 {
 
-    public List<GameObject> targets; 
-    private float spawnRate = 1.0f;
+    public List<GameObject> targets; // GameObjects in the "targets' list
+    private float spawnRate = 1.0f; // 'targets' spawn every 1 sec
 
-    
+    private int score; // Player's score
+    public TextMeshProUGUI scoreText; //score UI displayed on Screen
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         StartCoroutine(SpawnTarget()); //calling 'SpawnTarget' Coroutine
+        UpdateScore(0); //Score is zero upon game starting
+        
+    }
+
+    public void SwitchScene(int sceneIndex) // method to switch gamescenes
+    {
+        SceneManager.LoadScene(sceneIndex);
     }
 
     IEnumerator SpawnTarget() //return coroutine method to spawn targets
@@ -27,9 +42,16 @@ public class GameManger : MonoBehaviour
         
     }
 
+    public void UpdateScore(int scoreToAdd) // method to update score on game scene
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+
+    }
+
     // Update is called once per frame
     void Update()
     {
-       
+        
     }
 }
--- IN_Week6_Mobile_Game/Assets/Scripts/Target.cs
+++ IN_Week6_Mobile_Game/Assets/Scripts/Target.cs
@@ -1,27 +1,42 @@
 using System.Collections;
+using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
 
-    private Rigidbody targetRb; //All target items Rigidbodies
+    
+    private GameManger gameManager; // Access to GameManager.cs
 
-    private float minSpeed = 12;
-    private float maxSpeed = 16;
-    private float maxTorque = 4;
-    private float xRange = 4;
-    private float ySpawnPos = -6;
+    #region GameObjects: Targets
+    private Rigidbody targetRb; // Access to all target items' Rigidbodies
+
+    private float minSpeed = 12; // minimum speed
+    private float maxSpeed = 16; // maximum speed
+    private float maxTorque = 4; // maximum Torque(rotation)
+    private float xRange = 4; // range along the x axis
+    private float ySpawnPos = -6; // 'targets' spawn at a specified point on y axis
+
+    public int pointValue; // individual point values assigned to 'targets'
+
+    public ParticleSystem explosionParticle; // explosion particles for each target 
+    #endregion GameObjects: Targets
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        targetRb = GetComponent<Rigidbody>(); //retrieve rigidbodies of 'target' items
+
+        gameManager = GameObject.Find("Game_Manager").GetComponent<GameManger>(); // initialise access to GameManager GameObject
+
+        targetRb = GetComponent<Rigidbody>(); // intialise access to rigidbodies of 'target' items
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse); //random Y value
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse); // random XYZ value
 
         transform.position = RandomSpawnPos(); // position set as randomised X value
+
+        
     }
 
     Vector3 RandomForce() // return method call for random y value in start()
@@ -29,22 +44,24 @@ public class NewMonoBehaviourScript : MonoBehaviour
         return Vector3.up * Random.Range(minSpeed, maxSpeed); // = RandomForce()
     }
    
-    float RandomTorque() //return method call for random rotation values in XYZ
+    float RandomTorque() // return method call for random rotation values in XYZ
     {
         return Random.Range(-maxTorque, maxTorque);  // = RandomTorque()
     }
 
-    Vector3 RandomSpawnPos()  //return method call for random X value, set y value
+    Vector3 RandomSpawnPos()  // return method call for random X value, set y value
     {
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos); // = RandomSpawnPos
     }
 
-    private void OnMouseDown()
+    private void OnMouseDown()  // return method call for what should happen to targets upon mouse click
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) // call method for targets that fall into the collider sttached to the 'sensor' GameObject
     {
         Destroy(gameObject);
     }
@@ -52,6 +69,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    
 
     }
     
